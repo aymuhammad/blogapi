8b766c5faa88e922c230d3ec13f610530a688190
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-08 00:29:45 +0100

    add list view api

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api.serializer import PostSerializer
+from api.models import Post
+from rest_framework.views import APIView
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# list the post blog
+class ViewList(APIView):
+    def get(self, request):
+        blog = Post.objects.all()
+        serialzer = PostSerializer(blog, many=True)
+        return Response({'status':200, 'payload': serialzer.data})
+
