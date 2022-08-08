876f0966c9f3cc899b7098740fa36dedd76296f2
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-08 01:55:05 +0100

    update the views with register and login class

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from urllib import response
+from .serializer import RegisterSerializer
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
 
-# Create your views here.
+from blog.user import serializer
+
+
+# register API
+class Register(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data = request.data)
+        if not serializer.is_valid():
+            return Response({'payload': serializer.errors, 'message':'Something went Wrong'})
+        serializer.save()
+
+
+# login api
+class Login(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username is None or password is None:
+            return Response({'error':'Please provide both teh details'})
+        user = authenticate(request, username=username, password=password)
+
+        if not user:
+            return Response({'error': 'invalid Credential'})
+        token_obj,created = Token.objects.get_or_create(user=user)
+
+        return Response({"status": 200,'token':str(token_obj), "massage": "your are successfully logged in"})
