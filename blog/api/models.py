4e5f509b4cdf0be04290e4482125312439826ede
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-10 23:18:39 +0100

    add comment class

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,8 +1,6 @@
-from pyexpat import model
-from turtle import title
-from unicodedata import category
 from django.db import models
 from django.utils import timezone
+from .forms import CommentForm
 
 class Post(models.Model):
 
@@ -22,3 +20,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# to create a comment
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    # email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
