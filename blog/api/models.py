60988d2d4441993671b35ceca92c2f9404e1e51c
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-18 23:47:11 +0100

    add slug, author, date posted, updated, created and class tag

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,9 +1,16 @@
 import imp
+from statistics import mode
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
 from .forms import CommentForm
+
+class Tag(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
 
@@ -16,10 +23,14 @@ class Post(models.Model):
     )
 
     category = models.CharField(max_length=160, null=True, choices=Category, default='Wev-Devs')
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     description = models.TextField()
     content = models.TextField()
     date_posted = models.DateField(default=timezone.now)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
