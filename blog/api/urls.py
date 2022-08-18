d22c4ff7c08db2bfa985c1d416938f4d30ca0afc
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-19 00:04:12 +0100

    add slug to the urls

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.ViewList.as_view(), name='post_list'),
     path('add',views.AddPost.as_view(),name='Add_List'),
-    path('details/<str:pk>',views.PostDetails.as_view(), name='post_details'),
+    path('details/<str:pk>/<slug:slug>',views.PostDetails.as_view(), name='post_details'),
 ]
