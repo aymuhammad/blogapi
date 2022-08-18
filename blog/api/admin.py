0ef147548e285c35b2512d05e2824c5728fb07fe
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-19 00:05:51 +0100

    created class postadmin and class tag

--- blog/api/admin.py
+++ blog/api/admin.py
@@ -1,10 +1,8 @@
+from turtle import update
 from django.contrib import admin
-from .models import Post, Comment
-from .models import *
+from .models import Post, Tag
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+from .models import *
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'active')
@@ -13,3 +11,20 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on', 'update_on')
+    list_filter = ('tags', 'created_on', 'updated_on')
+    search_fields = ('title')
+    prepopulated_fields = {'slug': ('title', )}  # this create the slug field from the title field
+    autocomplete_fields = ('tags', )
+
+# Register your models here.
+admin.site.register(Post, PostAdmin)
+# admin.site.register(Comment)
+
+# TagAdmin must define "search_fields", because it's referenced by PostAdmin.autocomplete_fields.
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+
+admin.site.register(Tag, TagAdmin)
