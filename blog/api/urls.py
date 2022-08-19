c623a3529a6b822253d12e43356b29f588c04941
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-19 09:40:48 +0100

    add path comments

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.ViewList.as_view(), name='post_list'),
+    path('posts/', views.PostDetails.as_view(), name='details'),
     path('add',views.AddPost.as_view(),name='Add_List'),
-    path('details/<str:pk>/<slug:slug>',views.PostDetails.as_view(), name='post_details'),
+    path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>/', views.CommentDetail.as_view()),
 ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
