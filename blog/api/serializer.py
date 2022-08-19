cd0e716b31deec8700cb337ecc397e9847a99265
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-19 09:40:05 +0100

    add serializer post, comment and user

--- blog/api/serializer.py
+++ blog/api/serializer.py
@@ -1,9 +1,37 @@
 from dataclasses import field, fields
 from pyexpat import model
 from rest_framework import serializers
+
+from user.models import User
 from .models import Post
+
+from api.models import Comment  
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+# comment serializer
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'owner', 'comments']
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'posts', 'comments']
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'body', 'owner', 'post']
