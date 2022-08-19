256319f533d31d704e50c6128f064d5b408dbc40
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-19 09:53:46 +0100

    add auth path

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('api.urls')),
     path('', include('user.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
