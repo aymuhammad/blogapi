1c6534d29d164ec34a942026b0cce858bf116e65
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-19 09:36:36 +0100

    modified comment admin

--- blog/api/admin.py
+++ blog/api/admin.py
@@ -12,15 +12,8 @@ class CommentAdmin(admin.ModelAdmin):
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_on', 'update_on')
-    list_filter = ('tags', 'created_on', 'updated_on')
-    search_fields = ('title')
-    prepopulated_fields = {'slug': ('title', )}  # this create the slug field from the title field
-    autocomplete_fields = ('tags', )
-
 # Register your models here.
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post)
 # admin.site.register(Comment)
 
 # TagAdmin must define "search_fields", because it's referenced by PostAdmin.autocomplete_fields.
